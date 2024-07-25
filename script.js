481e78c5b739275e05bf5848141f7177d90e8987
Subin, Han <ella0411@naver.com>
2024-07-25 18:31:28 +0900

    add API

--- script.js
+++ script.js
@@ -15,36 +15,48 @@ document.addEventListener('DOMContentLoaded', () => {
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjBlMDFkYjdiZjJiMTFhMTRhZjQwYzcxZmJjZmQ1NyIsIm5iZiI6MTcyMTgwMDk3Ni42NzQ5NzEsInN1YiI6IjY2YTA3YTdlNmIzZmUyYjc0MzUwMzQwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0fCMIDCl4V3WzblCI-iupkhhCO9OaWMPHEIn0D9imWU'
             }
         };
-        const T_url ='https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1';
-    
-        const P_url = 'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1';
-    
-        fetch(P_url, options)
-        .then(response => response.json())
-        .then(response => {
-            let movieCard = response['results'];
+        const urls = [
+            'https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1',
+            'https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1',
+            'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1'
+        ]
 
+    
+        Promise.all(urls.map(url => fetch(url, options)))
+        .then(responses => Promise.all(responses.map(response => response.json())))
+        .then(dataArray => {
             let temp_html = "";
-            movieCard.forEach(item => {
-                let img_url = 'https://image.tmdb.org/t/p/w500' + item['backdrop_path'];
-                let title = item['title'];
-                let star = item['vote_average'];
-                let overview = item['overview'];
-                let id = item['id'];
+            const seenTitles = new Set();
+            
+            dataArray.forEach(response => {
+                let movieCard = response['results'];
 
-                temp_html += `
-                <div class="movie_card" id="${id}">
-                    <div class="content">
-                        <img src='${img_url}'>
-                        <div class="info">
-                            <h3>${title}</h3>
-                            <p class="star">⭐${star}</p>
-                            <p class="overview">${overview}</p>
+                movieCard.forEach(item => {
+                    let title = item['title'];
+                    if(seenTitles.has(title)) return;
+
+                    seenTitles.add(title);
+
+                    let img_url = 'https://image.tmdb.org/t/p/w500' + item['backdrop_path'];
+                    let star = item['vote_average'];
+                    let overview = item['overview'];
+                    let id = item['id'];
+
+                    temp_html += `
+                    <div class="movie_card" id="${id}">
+                        <div class="content">
+                            <img src='${img_url}'>
+                            <div class="info">
+                                <h3>${title}</h3>
+                                <p class="star">⭐${star}</p>
+                                <p class="overview">${overview}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                `;
-            });
+                    `;
+                });
+            })
+
             movie_list.innerHTML = temp_html;
             movie_card = document.getElementsByClassName('movie_card');
 
@@ -55,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
 
-            console.log(movieCard);
+            console.log(dataArray);
         })
         .catch(error => {
             // 에러가 발생하면 에러 메시지를 콘솔에 출력
